5a19fa152dfd90d2b856d1af9863e881a05ccc1f
a1lock <fogfogfogpak@mail.ru>
2026-05-24 12:35:49 +0300

    9 commit

--- backend/routes/contractRoutes.js
+++ backend/routes/contractRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { calculateContract } = require('../controllers/contractController');
+const { calculateContract, getContractHistory } = require('../controllers/contractController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 router.post('/calculate', verifyToken, calculateContract);
-router.get('/history', verifyToken, getContractHistory);
+router.get('/history', verifyToken, getContractHistory); // получение истории расчетов
 
 module.exports = router;
